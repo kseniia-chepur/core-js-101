f4d1965ecdb67b2eb9ab22e6cf0e44bd223f6384
kseniya.chepur <kseniya.chepur@gmail.com>
2022-12-27 21:36:03 +0200

    fix linter error

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -221,7 +221,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  return typeof +value === 'number' && isFinite(+value) ? +value : def;
+  return typeof +value === 'number' && Number.isFinite(+value) ? +value : def;
 }
 
 module.exports = {
